467e1270b39a09c5302cd4ab1521cb82e4a412b6
Kenny Evans <newtonsspawn@gmail.com>
2019-04-16 08:36:49 -0700

    Changed parsing format to ';;'

--- kenny/getoldtweets_test.py
+++ kenny/getoldtweets_test.py
@@ -13,17 +13,17 @@ tweetCriteria = got.manager.TweetCriteria()\
 
 outputFile = codecs.open("../data/hurricane_irma_tweets.csv", "w+", "utf-8")
 outputFile.write(
-    'username;date;retweets;favorites;text;geo;mentions;hashtags;id;permalink'
+    'username;;date;;retweets;;favorites;;text;;geo;;mentions;;hashtags;;id;;permalink'
 )
 print('Searching...\n')
 
 
 def receiveBuffer(tweets):
     for t in tweets:
-        outputFile.write(('\n%s;%s;%d;%d;"%s";%s;%s;%s;"%s";%s' % (
+        outputFile.write(('\n%s;;%s;;%d;;%d;;"%s";;%s;;%s;;%s;;"%s";;%s' % (
         t.username, t.date.strftime("%Y-%m-%d %H:%M"), t.retweets, t.favorites,
         t.text, t.geo, t.mentions, t.hashtags, t.id, t.permalink)))
-    outputFile.flush();
+    outputFile.flush()
     print('%d more saved on file...\n' % len(tweets))
 
 
